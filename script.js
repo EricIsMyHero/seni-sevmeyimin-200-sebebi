48aa0bdf090331cdee0d3ddc9497cf4d296d2cb9
EricIsMyHero <ericismyhero2467@gmail.com>
2025-08-11 18:08:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function shareSite() {
     url: window.location.href
   };
 function searchReasons() {
-  let input = document.getElementById("searchInput").value.toLowerCase();
+  let input = document.getElementById("searchInput").value.toLowerCase().trim();
   let reasons = document.querySelectorAll("#reasonList li");
-  
+
   reasons.forEach(reason => {
-    if (reason.textContent.toLowerCase().includes(input)) {
+    if (reason.textContent.toLowerCase().includes(input) || input === "") {
       reason.style.display = "list-item"; // göstər
     } else {
       reason.style.display = "none"; // gizlət
